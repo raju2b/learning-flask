e93b334d6b3c56ddcc6be286bd857a9fcdc94b8d
Sowri Raju Bathineni <raju.bathineni@gmail.com>
2018-11-04 00:19:29 +0530

    About Page Creation

--- LearningFlask.py
+++ LearningFlask.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
